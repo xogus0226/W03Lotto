496431b41fa1d5e84f38291029ddd3b175a8c94a
thk <qkdrhks123@gmail.com>
2023-09-21 16:08:12 +0900

    💄 세로 방향

--- app/src/main/java/kr/ac/kumoh/ce/s20180074/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180074/MainActivity.kt
@@ -2,10 +2,24 @@ package kr.ac.kumoh.ce.s20180074
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.kumoh.ce.s20180074.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        main = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_main)
+        setContentView(main.root)
+
+        main.btnGenerate.setOnClickListener{
+            main.num1.text = Random.nextInt(from = 1, until = 46).toString()
+            main.num2.text = Random.nextInt(from = 1, until = 46).toString()
+            main.num3.text = Random.nextInt(from = 1, until = 46).toString()
+            main.num4.text = Random.nextInt(from = 1, until = 46).toString()
+            main.num5.text = Random.nextInt(from = 1, until = 46).toString()
+            main.num6.text = Random.nextInt(from = 1, until = 46).toString()
+        }
     }
 }
